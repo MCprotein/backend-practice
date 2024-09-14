9c0e4ea558a3fa143485dabeb699d55635ad658c
MCProtein <chss3339@gmail.com>
2024-09-15 01:23:09 +0900

    refactor: dotenv -> node --env-file option 교체, 파일 구조 변경

--- data/seed.js
+++ data/seed.js
@@ -1,12 +1,10 @@
-import mongoose from 'mongoose';
-import data from './mock.js';
-import Product from '../models/Product.js';
-import * as dotenv from 'dotenv';
+import mongoose from 'mongoose'
+import data from './mock.js'
+import { Product } from '../src/models/Product.js'
 
-dotenv.config();
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.MONGODB_DATABASE_URL)
 
-await Product.deleteMany({}); // 인자로 삭제 조건을 전달
-await Product.insertMany(data); // 인자로 삽입할 데이터를 전달
+await Product.deleteMany({}) // 인자로 삭제 조건을 전달
+await Product.insertMany(data) // 인자로 삽입할 데이터를 전달
 
-mongoose.connection.close();
+mongoose.connection.close()
